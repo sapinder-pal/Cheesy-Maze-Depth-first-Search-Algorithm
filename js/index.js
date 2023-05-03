156939fbc139042e9cdbcd7eed748150ee1a6787
Sapinder Singh <sappiethird@gmail.com>
2023-05-03 11:30:57 -0400

    Refine layout & add Bootstrap

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-import Maze from "./mazeContainer.js";
+import Maze from './mazeContainer.js';
 
-const Canvas = document.querySelector("Canvas");
-const Ctx = Canvas.getContext("2d");
+const GameContainer = document.getElementById('game-container');
+const Canvas = document.getElementById('canvas');
+const Ctx = Canvas.getContext('2d');
 let Width, Height;
 // user-defined rowNum & colNum
-let CustomGrid = document.querySelectorAll("#rows, #columns");
+let CustomGrid = document.querySelectorAll('#rows, #columns');
 
 let CheesyMaze;
 let MazeRows = CustomGrid[0].value;
@@ -14,53 +15,39 @@ defineCanvasSize();
 initiate();
 
 /********************* CHANGING NUMBER OF ROWS & COLUMNS ****************/
-let ChangeBtn = document.querySelector("#change");
+let ChangeBtn = document.getElementById('change');
 
-ChangeBtn.addEventListener("click", _ => {
-	// if either of #rows and #columns has changed
-	if(CustomGrid[0].value !== MazeRows || CustomGrid[1].value !== MazeColumns) {
+ChangeBtn.addEventListener('click', _ => {
+  // if either of #rows and #columns has changed
+  if (CustomGrid[0].value !== MazeRows || CustomGrid[1].value !== MazeColumns) {
+    resetRowCol();
+    Ctx.clearRect(0, 0, Width, Width);
+    unListenMoves();
 
-		resetRowCol();
-		Ctx.clearRect(0, 0, Width, Width);
-		unListenMoves();
-
-		// restart
-		initiate();
-	}
+    // restart
+    initiate();
+  }
 });
-
-
 
 /***************************************** GLOBAL FUNCTIONS ********************************************/
 
 function defineCanvasSize() {
-	if (window.innerWidth >= 650) {
-		Width = 600; Height = 600;
-	}
-	else if (450 <= window.innerWidth && window.innerWidth < 650) {
-		Width = window.innerWidth - 10; Height = 500;
-	}
-	else {
-		Width = window.innerWidth - 10;
-		Height = 460;
-	}
+  // get width of GameContainer and assign it to Width and Height.
+  Width = Height = GameContainer.offsetWidth;
 }
 
 // Create CheesyMaze
 function initiate() {
-	CheesyMaze = new Maze(Ctx, Width, Height, MazeRows, MazeColumns);
-	CheesyMaze.setup();
-	listenMoves();
+  CheesyMaze = new Maze(Ctx, Width, Height, MazeRows, MazeColumns);
+  CheesyMaze.setup();
+  listenMoves();
 }
 
 // Reset MazeRows & MazeColumns
 function resetRowCol() {
-	MazeRows = CustomGrid[0].value;
-	MazeColumns = CustomGrid[1].value;
+  MazeRows = CustomGrid[0].value;
+  MazeColumns = CustomGrid[1].value;
 }
-
-
-
 
 /******************************************** MOVE EVENTS ***************************************************************/
 
@@ -68,127 +55,132 @@ let GestureStartX, GestureStartY;
 let GestureEndX, GestureEndY;
 
 function listenMoves() {
-	window.addEventListener("keydown", handleKeyDown);
-	Canvas.addEventListener("touchstart", handleGestureStart);
-	Canvas.addEventListener("mousedown", handleGestureStart);
-	
+  window.addEventListener('keydown', handleKeyDown);
+  Canvas.addEventListener('touchstart', handleGestureStart);
+  Canvas.addEventListener('mousedown', handleGestureStart);
 }
 
 function unListenMoves() {
-	window.removeEventListener("keydown", handleKeyDown);
-	Canvas.removeEventListener("touchstart", handleGestureStart);
-	Canvas.removeEventListener("mousedown", handleGestureStart);
+  window.removeEventListener('keydown', handleKeyDown);
+  Canvas.removeEventListener('touchstart', handleGestureStart);
+  Canvas.removeEventListener('mousedown', handleGestureStart);
 }
-
 
 // EVENT HANDLERS
 function handleKeyDown(evt) {
-	let Player = CheesyMaze.player;
-	Player.move({ keyCode: evt.keyCode });
+  let Player = CheesyMaze.player;
+  Player.move({ keyCode: evt.keyCode });
 
-	checkCompletion();
+  checkCompletion();
 }
 
 function handleGestureStart(evt) {
-	evt.preventDefault();
+  evt.preventDefault();
 
-	/* 	Handle TouchStart */
-	if (evt.type === "touchstart") {
-		if (evt.touches && evt.touches.length > 1) return;	// exit function if multi-touch occurred
+  /* 	Handle TouchStart */
+  if (evt.type === 'touchstart') {
+    if (evt.touches && evt.touches.length > 1) return; // exit function if multi-touch occurred
 
-		GestureStartX = evt.targetTouches[0].pageX - Canvas.offsetLeft;
-		GestureStartY = evt.targetTouches[0].pageY - Canvas.offsetTop;
+    GestureStartX = evt.targetTouches[0].pageX - Canvas.offsetLeft;
+    GestureStartY = evt.targetTouches[0].pageY - Canvas.offsetTop;
 
-		Canvas.addEventListener("touchend", handleGestureEnd);
-	}
+    Canvas.addEventListener('touchend', handleGestureEnd);
+  } else {
+    /* Handle MouseDown */
+    GestureStartX = evt.pageX - Canvas.offsetLeft;
+    GestureStartY = evt.pageY - Canvas.offsetTop;
 
-	/* Handle MouseDown */
-	else {
-		GestureStartX = evt.pageX - Canvas.offsetLeft;
-		GestureStartY = evt.pageY - Canvas.offsetTop;
-
-		Canvas.addEventListener("mouseup", handleGestureEnd);
-	}
+    Canvas.addEventListener('mouseup', handleGestureEnd);
+  }
 }
 
 function handleGestureEnd(evt) {
+  /* Handle TouchEnd */
+  if (evt.type === 'touchend') {
+    GestureEndX = evt.changedTouches[0].pageX - Canvas.offsetLeft;
+    GestureEndY = evt.changedTouches[0].pageY - Canvas.offsetTop;
+  } else {
+    /* Handle MouseUp */
+    GestureEndX = evt.pageX - Canvas.offsetLeft;
+    GestureEndY = evt.pageY - Canvas.offsetTop;
+  }
 
-	/* Handle TouchEnd */
-	if (evt.type === "touchend") {
-		GestureEndX = evt.changedTouches[0].pageX - Canvas.offsetLeft;
-		GestureEndY = evt.changedTouches[0].pageY - Canvas.offsetTop;
-	}
+  let Player = CheesyMaze.player;
+  // Cell Range
+  let PlayerRangeX = Player.xCord + Player.width;
+  let PlayerRangeY = Player.yCord + Player.height;
 
-	/* Handle MouseUp */
-	else {
-		GestureEndX = evt.pageX - Canvas.offsetLeft;
-		GestureEndY = evt.pageY - Canvas.offsetTop;
-	}
+  // Check gesture occurred on Player cell
+  let isPlayerCol =
+    GestureStartX >= Player.xCord && GestureStartX <= PlayerRangeX;
+  let isPlayerRow =
+    GestureStartY >= Player.yCord && GestureStartY <= PlayerRangeY;
+  let gestureOnPlayer = isPlayerCol && isPlayerRow;
 
-	let Player = CheesyMaze.player;
-	// Cell Range
-	let PlayerRangeX = Player.xCord + Player.width;
-	let PlayerRangeY = Player.yCord + Player.height;
+  if (gestureOnPlayer) {
+    // Check if target is either of Player's neighbor
+    let targetColumn = Math.floor(GestureEndX / CheesyMaze.cellWidth);
+    let targetRow = Math.floor(GestureEndY / CheesyMaze.cellHeight);
 
-	// Check gesture occurred on Player cell
-	let isPlayerCol = (GestureStartX >= Player.xCord) && (GestureStartX <= PlayerRangeX);
-	let isPlayerRow = (GestureStartY >= Player.yCord) && (GestureStartY <= PlayerRangeY);
-	let gestureOnPlayer = isPlayerCol && isPlayerRow;
+    let possibleTargets = {
+      left:
+        Player.colNum !== 0
+          ? CheesyMaze.grid[Player.rowNum][Player.colNum - 1]
+          : undefined,
 
-	if (gestureOnPlayer) {
-		// Check if target is either of Player's neighbor
-		let targetColumn = Math.floor(GestureEndX / CheesyMaze.cellWidth);
-		let targetRow = Math.floor(GestureEndY / CheesyMaze.cellHeight);
+      top:
+        Player.rowNum !== 0
+          ? CheesyMaze.grid[Player.rowNum - 1][Player.colNum]
+          : undefined,
 
-		let possibleTargets = {
-			left: Player.colNum !== 0 ? (CheesyMaze.grid[Player.rowNum][Player.colNum - 1]) : undefined,
+      right:
+        Player.colNum !== CheesyMaze.gridLastColumn
+          ? CheesyMaze.grid[Player.rowNum][Player.colNum + 1]
+          : undefined,
 
-			top: Player.rowNum !== 0 ? (CheesyMaze.grid[Player.rowNum - 1][Player.colNum]) : undefined,
+      bottom:
+        Player.rowNum !== CheesyMaze.gridLastRow
+          ? CheesyMaze.grid[Player.rowNum + 1][Player.colNum]
+          : undefined,
+    };
 
-			right: Player.colNum !== CheesyMaze.gridLastColumn ? (CheesyMaze.grid[Player.rowNum][Player.colNum + 1]) : undefined,
+    Player.move(possibleTargets, CheesyMaze.grid[targetRow][targetColumn]);
+  }
 
-			bottom: Player.rowNum !== CheesyMaze.gridLastRow ? (CheesyMaze.grid[Player.rowNum + 1][Player.colNum]) : undefined
-		}
-
-		Player.move(possibleTargets, CheesyMaze.grid[targetRow][targetColumn]);
-	}
-
-	checkCompletion();
+  checkCompletion();
 }
-
-
 
 /***************************************** GAME COMPLETION *****************************************************/
 
-let CompletionBox = document.querySelector(".game-complete");
-let RestartBtn = document.querySelector("#restart");
-
+let CompletionBox = document.querySelector('.game-complete');
+let RestartBtn = document.querySelector('#restart');
 
 function checkCompletion() {
-	let Player = CheesyMaze.player;
-	let reachedCol = Player.colNum === CheesyMaze.goal.colNum;
-	let reachedRow = Player.rowNum === CheesyMaze.goal.rowNum;
+  let Player = CheesyMaze.player;
+  let reachedCol = Player.colNum === CheesyMaze.goal.colNum;
+  let reachedRow = Player.rowNum === CheesyMaze.goal.rowNum;
 
-	if (reachedRow && reachedCol) {
-		unListenMoves();
-		gameComplete();
-	}
+  if (reachedRow && reachedCol) {
+    unListenMoves();
+    gameComplete();
+  }
 }
 function gameComplete() {
-	let Player = CheesyMaze.player;
-	// Subtracted 1 from Player.stepCount because first step counts when Player was drawn up
-	document.querySelector(".game-complete h2").innerText =
-		`You've moved ${Player.stepCount - 1} steps to get the Cheese`;
+  let Player = CheesyMaze.player;
+  // Subtracted 1 from Player.stepCount because first step counts when Player was drawn up
+  document.querySelector('.game-complete .steps-count').innerText = String(
+    Player.stepCount - 1
+  );
 
-	CompletionBox.classList.add("show");
-	RestartBtn.addEventListener("click", restart);
+  CompletionBox.classList.add('show');
+  RestartBtn.addEventListener('click', restart);
 }
 
 // Restart Game
 function restart() {
-	CompletionBox.classList.remove("show");
-	initiate();
-	document.querySelector(".game-complete h2").innerText = "";
+  CompletionBox.classList.remove('show');
+  initiate();
+  document.querySelector('.game-complete h2').innerText = '';
 
-	RestartBtn.removeEventListener("click", restart);
+  RestartBtn.removeEventListener('click', restart);
 }
--- js/mazeContainer.js
+++ js/mazeContainer.js
@@ -1,102 +1,118 @@
-import Cell from "./cell.js";
-import Player from "./player.js";
+import Cell from './cell.js';
+import Player from './player.js';
 
-export default class Maze {	
-	constructor(ctx, width, height, rows, columns) {
-		this.ctx = ctx;
-		this.width = width;
-		this.height = height;
-		this.rows = rows;
-		this.columns = columns;
-		this.cellWidth = width / columns;
-		this.cellHeight = height / rows;
+export default class Maze {
+  constructor(ctx, width, height, rows, columns) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+    this.rows = rows;
+    this.columns = columns;
+    this.cellWidth = width / columns;
+    this.cellHeight = height / rows;
 
-		this.grid = []; // to store individual cells
-		this.stack = []; // to push each visited cell for tracking previous steps
-	}
+    this.grid = []; // to store individual cells
+    this.stack = []; // to push each visited cell for tracking previous steps
+  }
 
-	//define grid
-	setup() {
-		for (let rowNum = 0; rowNum < this.rows; rowNum++) {
-			let row = [];
+  //define grid
+  setup() {
+    for (let rowNum = 0; rowNum < this.rows; rowNum++) {
+      let row = [];
 
-			for (let colNum = 0; colNum < this.columns; colNum++) {
-				let cell = new Cell(this.ctx, rowNum, colNum, this.cellWidth, this.cellHeight);
-				row.push(cell);
-			}
-			this.grid.push(row);
-		}
-		// will be used to place player diagonally opposite to goal
-		this.gridLastRow = this.grid.length - 1;
-		this.gridLastColumn = this.grid[0].length - 1;
+      for (let colNum = 0; colNum < this.columns; colNum++) {
+        let cell = new Cell(
+          this.ctx,
+          rowNum,
+          colNum,
+          this.cellWidth,
+          this.cellHeight
+        );
+        row.push(cell);
+      }
+      this.grid.push(row);
+    }
+    // will be used to place player diagonally opposite to goal
+    this.gridLastRow = this.grid.length - 1;
+    this.gridLastColumn = this.grid[0].length - 1;
 
-		this.player = new Player(this);
+    this.player = new Player(this);
 
-		// show preparing-stuff
-		document.querySelector(".preparing-grid").classList.add("show");
+    // show preparing-stuff
+    document.querySelector('.preparing-grid').classList.add('show');
 
-		//set random starting point
-		this.currentCell = this.startPoint();
-		this.drawMap();
-	}
+    //set random starting point
+    this.currentCell = this.startPoint();
+    this.drawMap();
+  }
 
+  drawMap() {
+    let canvas = document.getElementById('canvas');
+    canvas.width = this.width;
+    canvas.height = this.height;
 
-	drawMap() {
-		let canvas = document.querySelector("canvas");
-		canvas.width = this.width;
-		canvas.height = this.height;
+    this.currentCell.visited = true;
+    this.grid.forEach(row => row.forEach(col => col.drawCell()));
 
-		this.currentCell.visited = true;
-		this.grid.forEach(row => (row.forEach(col => col.drawCell())));
+    let nextCell = this.currentCell.next(this.grid);
+    if (nextCell) {
+      nextCell.visited = true;
 
-		let nextCell = this.currentCell.next(this.grid);
-		if (nextCell) {
-			nextCell.visited = true;
-			
-			this.stack.push(this.currentCell);
-			this.currentCell.removeWalls(nextCell);
-			this.currentCell = nextCell;
-		}
+      this.stack.push(this.currentCell);
+      this.currentCell.removeWalls(nextCell);
+      this.currentCell = nextCell;
+    }
 
-		// else if we can go back
-		else if (this.stack.length > 0) {
-			this.currentCell = this.stack.pop();
-		}
-		
-		// if can't go back, set goal & player
-		if (this.stack.length === 0) {
-			this.goal = this.currentCell;
-			this.drawGoal(this.goal);
+    // else if we can go back
+    else if (this.stack.length > 0) {
+      this.currentCell = this.stack.pop();
+    }
 
-			// set player
-			this.player.setPlayer();
-			// remove Preparing Screen
-			document.querySelector(".preparing-grid").classList.remove("show");
-			return;
-		}
-		
-		window.requestAnimationFrame(_ => this.drawMap());
-	}
+    // if can't go back, set goal & player
+    if (this.stack.length === 0) {
+      this.goal = this.currentCell;
+      this.drawGoal(this.goal);
 
-	// point to start drawing cell (either of four corners)
-	startPoint() {
-		let corners = [this.grid[0][0], this.grid[this.gridLastRow][0], this.grid[0][this.gridLastColumn],
-		this.grid[this.gridLastRow][this.gridLastColumn]];
+      // set player
+      this.player.setPlayer();
+      // remove Preparing Screen
+      document.querySelector('.preparing-grid').classList.remove('show');
+      return;
+    }
 
-		return corners[Math.floor(Math.random() * 4)];
-	}
+    window.requestAnimationFrame(_ => this.drawMap());
+  }
 
-	drawGoal(goal) {
-		let cheese = new Image();
+  // point to start drawing cell (either of four corners)
+  startPoint() {
+    let corners = [
+      this.grid[0][0],
+      this.grid[this.gridLastRow][0],
+      this.grid[0][this.gridLastColumn],
+      this.grid[this.gridLastRow][this.gridLastColumn],
+    ];
 
-		cheese.width = goal.width * 2;			// scale cheese inside goal cell
-		cheese.height = goal.height * 2;
+    return corners[Math.floor(Math.random() * 4)];
+  }
 
-		// adjust cheese position according to its size
-		cheese.xPos = goal.xCord - cheese.width/4;
-		cheese.yPos = goal.yCord - cheese.height/8;
+  drawGoal(goal) {
+    let cheese = new Image();
 
-		cheese.onload = ()=> goal.ctx.drawImage(cheese, cheese.xPos, cheese.yPos, cheese.width, cheese.height);
-		cheese.src = "./images/cheese.svg";
-	}
+    cheese.width = goal.width * 2; // scale cheese inside goal cell
+    cheese.height = goal.height * 2;
+
+    // adjust cheese position according to its size
+    cheese.xPos = goal.xCord - cheese.width / 4;
+    cheese.yPos = goal.yCord - cheese.height / 8;
+
+    cheese.onload = () =>
+      goal.ctx.drawImage(
+        cheese,
+        cheese.xPos,
+        cheese.yPos,
+        cheese.width,
+        cheese.height
+      );
+    cheese.src = './images/cheese.svg';
+  }
 }
